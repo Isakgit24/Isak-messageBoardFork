2a7da34b59c683e94e1fc3bd732fc3c0b34a09d4
Charlie <challe-1992@hotmail.com>
2026-03-18 11:05:27 +0100

    base!

---
+++ js/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBYrqmRZmIQjtWQZMSynD0F2wZPWA462tc',
+  authDomain: 'messageboard-77286.firebaseapp.com',
+  databaseURL:
+    'https://messageboard-77286-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'messageboard-77286',
+  storageBucket: 'messageboard-77286.firebasestorage.app',
+  messagingSenderId: '262402990271',
+  appId: '1:262402990271:web:724586d400e56438b0a60a'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
